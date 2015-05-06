82a378432e8467ae2a2579465ae6792238857297
avinashkunuje <itsavi.ks@gmail.com>
2015-05-06 19:23:09 +0530

    created model and database

--- hackernews/stories/models.py
+++ hackernews/stories/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from urlparse import urlparse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Story(models.Model):
+	title=models.CharField(max_length=200)
+	url=models.URLField()
+	#domain
+	points=models.IntegerField()
+	moderator=models.ForeignKey(User)
+	created_at=models.DateTimeField(auto_now_add=True)
+	updated_at=models.DateTimeField(auto_now=True)
+
+	@property
+	def domain(self):
+		return urlparse(self,url).netloc
+
